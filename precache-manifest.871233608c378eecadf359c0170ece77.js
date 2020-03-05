675654ce55b83e2c4e067b5c36dd3d9d9ff361ca
Natalia_Konovalenko <natalia_konovalenko@epam.com>
2020-03-05 12:44:54 +0200

    Updates

---
+++ precache-manifest.871233608c378eecadf359c0170ece77.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9ed9a660492f62867304a5de5d4e00",
+    "url": "/demo-react-sn/index.html"
+  },
+  {
+    "revision": "8da6cf1c11e8722ff61c",
+    "url": "/demo-react-sn/static/css/3.e4a05fbd.chunk.css"
+  },
+  {
+    "revision": "cf756e2633c0963a635b",
+    "url": "/demo-react-sn/static/css/4.06e13947.chunk.css"
+  },
+  {
+    "revision": "5dc6128613524374d806",
+    "url": "/demo-react-sn/static/css/main.6af3886d.chunk.css"
+  },
+  {
+    "revision": "2572c9e6a6eaad568387",
+    "url": "/demo-react-sn/static/js/2.95a93caa.chunk.js"
+  },
+  {
+    "revision": "8da6cf1c11e8722ff61c",
+    "url": "/demo-react-sn/static/js/3.16d5216a.chunk.js"
+  },
+  {
+    "revision": "cf756e2633c0963a635b",
+    "url": "/demo-react-sn/static/js/4.ad13b38a.chunk.js"
+  },
+  {
+    "revision": "5dc6128613524374d806",
+    "url": "/demo-react-sn/static/js/main.04084579.chunk.js"
+  },
+  {
+    "revision": "2b67b8f6247ce100bcea",
+    "url": "/demo-react-sn/static/js/runtime-main.a62a0b47.js"
+  },
+  {
+    "revision": "16d14331e2d5c98dd54c026d8609a13a",
+    "url": "/demo-react-sn/static/media/preloader.16d14331.svg"
+  }
+]);
